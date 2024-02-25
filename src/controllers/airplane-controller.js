e514efd715ff46c8ee643371bf925bbd9874d6a2
hassanrahim26 <hassanrahim26@gmail.com>
2024-02-25 13:33:37 +0530

    Added get all airplanes api

---
+++ src/controllers/airplane-controller.js
@@ -0,0 +1,48 @@
+const { StatusCodes } = require('http-status-codes');
+
+const { AirplaneService } = require('../services/');
+const { SuccessResponse, ErrorResponse } = require('../utils/common')
+
+/**
+ * POST: /airplanes
+ * req-body {modelNumber: 'airbus a320', capacity: 520}
+ */
+
+async function createAirplane(req, res) {
+    try{
+        //console.log(req.body);
+        const airplane = await AirplaneService.createAirplane({
+            modelNumber: req.body.modelNumber,
+            capacity: req.body.capacity
+        });
+        SuccessResponse.data = airplane;
+        return res
+                .status(StatusCodes.CREATED)
+                .json(SuccessResponse);
+    } catch(error){
+        ErrorResponse.error = error;
+        return res
+                .status(error.statusCode)
+                .json(ErrorResponse);
+    }
+}
+
+async function getAirplanes(req, res) {
+    try {
+        const airplanes = await AirplaneService.getAirplanes();
+        SuccessResponse.data = airplanes;
+        return res
+                .status(StatusCodes.OK)
+                .json(SuccessResponse);
+    } catch (error) {
+        ErrorResponse.error = error;
+        return res
+                .status(error.StatusCodes)
+                .json(ErrorResponse);
+    }
+}
+
+module.exports = {
+    createAirplane,
+    getAirplanes
+}
