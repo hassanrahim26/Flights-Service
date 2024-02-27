777d9bbd1f78b483b832445b42a1674d1153b1cc
hassanrahim26 <hassanrahim26@gmail.com>
2024-02-27 14:31:43 +0530

    added a new city model using the following command:
    ```
    npx sequelize model:generate --name City --attributes name:String
    npx sequelize db:migrate
    ```

---
+++ src/migrations/20240227083212-create-city.js
@@ -0,0 +1,32 @@
+'use strict';
+var DataTypes = require('sequelize/lib/data-types');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Cities', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Cities');
+  }
+};
