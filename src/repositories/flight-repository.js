3701e0b4d5332ad6788a1fbbfb4add3c2dd5643d
hassanrahim26 <hassanrahim26@gmail.com>
2024-03-05 20:20:31 +0530

    Added transaction to update seats

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -52,15 +52,22 @@ class FlightRepository extends CrudRepository{
     }
 
     async updateRemainingSeats(flightId, seats, dec = true) {
-        await db.sequelize.query(addRowLockOnFlights(flightId));
-        const flight = await Flight.findByPk(flightId);
-        if(+dec){
-            await flight.decrement('totalSeats', {by: seats});
+        const transaction = await db.sequelize.transaction();
+        try {
+            await db.sequelize.query(addRowLockOnFlights(flightId));
+            const flight = await Flight.findByPk(flightId);
+            if(+dec){
+                await flight.decrement('totalSeats', {by: seats}, {transaction: transaction});
+            }
+            else {
+                await flight.increment('totalSeats', {by: seats}, {transaction: transaction});
+            }
+            await transaction.commit;
+            return flight;
+        } catch (error) {
+            await transaction.rollback();
+            throw error;
         }
-        else {
-            await flight.increment('totalSeats', {by: seats});
-        }
-        return flight;
     }
 }
 
